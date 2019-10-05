07e0fc3c7cf48cebf64adecb4d29aaa8c154e232
lleic12101 <50154894+lleic12101@users.noreply.github.com>
2019-10-05 20:14:23 +0300

    newWebchat

--- meconnect/js/main.js
+++ meconnect/js/main.js
@@ -39,3 +39,52 @@ $('.main__serviceItemCheckboxInput').click(function(){
 	}
 	$('.main__servicePayPrice').html(priceSumm);
 });
+
+$('.main__allChatsItem').click(function(){
+	$('.main__allChatsItem').removeClass('main__allChatsItem-active');
+	$('.main__chatMessagesBlock').removeClass('main__chatMessagesBlock-active');
+	$(this).addClass('main__allChatsItem-active');
+	if($(this).hasClass('main__allChatsItem-newMsg')){
+		$(this).removeClass('main__allChatsItem-newMsg');
+	}
+	var objs = document.querySelectorAll('.main__chatMessagesBlock');
+	for(var i = 0; i < objs.length; i++){
+		if(objs[i].id == this.id){
+			objs[i].classList.add('main__chatMessagesBlock-active');
+			$('.main__backToChatListBlock').addClass('main__backToChatListBlock-active');
+			if (window.innerWidth <= 1024) {
+				$('.main__block-chat').show();
+				var img = this.querySelector('.main__allChatsImg');
+				var imgSrc = $(img).attr("src");;
+				$('.main__BackToChatImg').attr("src", imgSrc);
+				var name = this.querySelector('.main__allChatsName').innerHTML;
+				var name = name.split(' ')[0];
+				$('.main__backToChatName').html(name);
+			}
+			return;
+		}
+	}
+});
+
+$('.main__chatIconCircle').click(function(){
+	$(this).toggleClass('main__chatIconCircle-active');
+	$('.main__block-currentPage').slideToggle();
+});
+
+$('.main__backToChatIcon').click(function(){
+	$('.main__block-chat').hide();
+	$('.main__backToChatListBlock').removeClass('main__backToChatListBlock-active');
+});
+
+containerSize();
+window.onresize = function() {
+	containerSize();	
+}
+
+function containerSize(){
+	if (window.innerWidth <= 1024) {
+		var width = $('.main__block-allChats').outerWidth();
+		$('.main__block-currentPage').outerWidth(width);
+		$('.main__block-chat').outerWidth(width);
+	}
+}
