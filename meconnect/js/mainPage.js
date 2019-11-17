ff00536edb9e19707633ab0bf25aa9d9ecfd9aa4
lleic12101 <50154894+lleic12101@users.noreply.github.com>
2019-11-17 18:06:09 +0200

    update

---
+++ meconnect/js/mainPage.js
@@ -0,0 +1,21 @@
+﻿(function (){
+	link = $('.js-scroll-link');
+	link.on('click', function () {
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
+})();
+
+$('.main__secondBlockToggleItem').click(function(){
+	$(this).find('.main__secondBlockToggleImgBlock-more').toggleClass('main__secondBlockToggleImg-opacity');
+	$(this).find('.main__secondBlockToggleImgBlock-less').toggleClass('main__secondBlockToggleImg-opacity');
+	$(this).find('.main__secondBlockToggleItem-leftCircle').slideToggle();
+	$(this).find('.main__secondBlockToggleItem-rightCircle').slideToggle();
+	$(this).find('.main__secondBlockToggleItemText').slideToggle();
+});
+
+$('.ham').click(function(){
+	this.classList.toggle('active');
+	$('.header__navWrapper').slideToggle();
+});
