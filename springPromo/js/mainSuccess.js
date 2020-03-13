cfc4d5b078267a80442525d9b139e49a2dee1361
lleic12101 <50154894+lleic12101@users.noreply.github.com>
2020-03-13 15:47:09 +0200

    deleted

--- springPromo/js/mainSuccess.js
+++
@@ -1,7 +0,0 @@
-﻿//Copy to clipboard
-var btns = document.querySelectorAll('.success__dataCopyBtn');
-for(var i = 0; i < btns.length; i++){
-    btns[i].addEventListener('click', function(){
-        navigator.clipboard.writeText(this.parentNode.querySelector('.success__dataInput').value);
-    });
-}
