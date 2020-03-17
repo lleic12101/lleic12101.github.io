6b6aa022724aa07ebc4fa061075eae443293f6a8
lleic12101 <50154894+lleic12101@users.noreply.github.com>
2020-03-17 14:34:41 +0200

    cryptoSmartDriveCabinet

---
+++ cryptoSmartDriveCabinet/js/main.js
@@ -0,0 +1,18 @@
+﻿var doc = document;
+
+//Cabinet ways tabs
+var waysBtn = doc.querySelectorAll('.main__contentWayHover');
+for(var i = 0; i < waysBtn.length; i++) {
+    waysBtn[i].addEventListener("click", function () {
+        $('.main__contentWay').removeClass('main__contentWay-active');
+        $(this).parent().addClass('main__contentWay-active');
+        var id = this.parentNode.id.substr(0, this.parentNode.id.length - 3);
+        $('.main__contentWayContent').removeClass('main__contentWayContent-active');
+        $('#'+id).addClass('main__contentWayContent-active');
+    });
+}
+
+//Burger menu
+doc.querySelector('.hamburger').addEventListener("click", function () {
+   $('.main__asideItemsSlideBlock').slideToggle();
+});
