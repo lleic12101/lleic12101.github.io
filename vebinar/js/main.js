9eab4003757cdfb415d26037ad3afa841b9b41d6
lleic12101 <50154894+lleic12101@users.noreply.github.com>
2020-04-01 14:23:51 +0300

    updatedAgain

--- vebinar/js/main.js
+++ vebinar/js/main.js
@@ -6,3 +6,8 @@ if(window.innerWidth > 736){
     var chatBodyHeight = (24 + video.clientHeight + watcherBlock.clientHeight) - chatHeader.clientHeight - chatFooter.clientHeight - 4;
     chatBodyWatcher.style.height = chatBodyHeight + 'px';
 }
+
+var callBtn = document.querySelector('.watcher__button-call');
+callBtn.addEventListener("click", function () {
+   document.querySelector('.watcher__buttonBlockText-dots').style.opacity = 1;
+});
