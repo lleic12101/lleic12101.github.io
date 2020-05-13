32bec90d5978592cb13a9866f07595f989e7a265
lleic12101 <50154894+lleic12101@users.noreply.github.com>
2020-05-13 14:20:23 +0300

    solUpdate

--- SOL_sexopatology/js/main.js
+++ SOL_sexopatology/js/main.js
@@ -1,1 +1,31 @@
-﻿
+﻿(function (){
+    link = $('.js-scroll-link');
+    link.on('click', function () {
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
+})();
+
+var modal = document.querySelector('.modal');
+function toggleModal() {
+    modal.classList.toggle('show-modal');
+}
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+window.addEventListener("click", windowOnClick);
+
+$('.footer__politic').click(function () {
+   $('.modal__contentAccept').hide();
+   $('.modal__contentConfidential').show();
+   toggleModal();
+});
+
+$('.footer__accept').click(function () {
+    $('.modal__contentAccept').show();
+    $('.modal__contentConfidential').hide();
+    toggleModal();
+});
