06722fb56b254b9a69be14a8dffe198ad3156f6b
lleic12101 <50154894+lleic12101@users.noreply.github.com>
2020-09-12 01:51:04 +0300

    indexPageUpdate

--- vet_clinic/js/main.js
+++ vet_clinic/js/main.js
@@ -13,10 +13,12 @@
     }
 });
 
+
+//map
 var map;
 var marker;
 function initMap() {
-    var myLatLng = {lat: 47.107716, lng: 37.642368};
+    var myLatLng = {lat: 47.107635, lng: 37.642391};
 
     map = new google.maps.Map(document.getElementById('map'), {
         center: myLatLng,
@@ -25,7 +27,59 @@ function initMap() {
 
     marker = new google.maps.Marker({
         position: myLatLng,
+        title: 'Орджонікідзевська ветеринарна клініка',
         map: map,
         icon: 'img/contacts/point.png'
     });
+}
+
+if($('*').is('.main__indexArticleSlides')){
+	//article slider
+	$('.main__indexArticleSlides').slick({
+		dots: false,
+		infinite: true,
+		speed: 300,
+		centerMode: true,
+		slidesToShow: 3,
+		variableWidth: true,
+		draggable: false,
+		prevArrow: $('.main__indexArticleControlsArrow-prev'),
+		nextArrow: $('.main__indexArticleControlsArrow-next')
+	});
+
+	$('.main__indexArticleSlides').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+		$(slick.$slides[nextSlide]).addClass('main__articleContentMoreArticle-slide-active');
+		$(slick.$slides[currentSlide]).removeClass('main__articleContentMoreArticle-slide-active');
+		$('.main__indexArticleControlsNum-bold').html(nextSlide + 1);
+	});
+
+	var slides = document.querySelectorAll('.main__articleContentMoreArticle-slide');
+	slides[4].classList.add('main__articleContentMoreArticle-slide-active');
+
+	//services slider
+	$('.main__indexServicesTabButtons').slick({
+		slidesToShow: 6,
+		slidesToScroll: 1,
+		asNavFor: '.main__indexServicesTabs',
+		dots: false,
+		centerMode: false,
+		focusOnSelect: true,
+		adaptiveHeight: true,
+		prevArrow: $('.main__indexServicesArrow-prev'),
+		nextArrow: $('.main__indexServicesArrow-next')
+	});
+	$('.main__indexServicesTabButtons').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+		$(slick.$slides[nextSlide]).addClass('main__indexServicesTabButton-active');
+		$(slick.$slides[currentSlide]).removeClass('main__indexServicesTabButton-active');
+	});
+	var slides2 = document.querySelectorAll('.main__indexServicesTabButton');
+	slides2[6].classList.add('main__indexServicesTabButton-active');
+
+	$('.main__indexServicesTabs').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		fade: true,
+		asNavFor: '.main__indexServicesTabButtons'
+	});
 }
