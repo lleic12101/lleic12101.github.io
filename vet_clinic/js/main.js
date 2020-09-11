b339c9dd87215f92d071a49f1f1b8d64fc4b6644
lleic12101 <50154894+lleic12101@users.noreply.github.com>
2020-09-11 13:42:34 +0300

    faq

--- vet_clinic/js/main.js
+++ vet_clinic/js/main.js
@@ -1,1 +1,14 @@
-﻿
+﻿$(".main__faqTab").click(function () {
+    if(!$(this).hasClass('main__faqTab-wait'))
+    {
+        if($(this).hasClass('main__faqTab-active')){
+            $(this).find('.main__faqTabText').slideUp();
+            $(this).find('.main__faqTabContentArrow').removeClass('main__faqTabContentArrow-active');
+            $(this).removeClass('main__faqTab-active');
+        } else {
+            $(this).find('.main__faqTabText').slideDown();
+            $(this).find('.main__faqTabContentArrow').addClass('main__faqTabContentArrow-active');
+            $(this).addClass('main__faqTab-active');
+        }
+    }
+});
