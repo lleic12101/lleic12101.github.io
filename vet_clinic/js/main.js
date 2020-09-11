0f64aa461362bba8624eb3418b9dc5bfe834b5cc
lleic12101 <50154894+lleic12101@users.noreply.github.com>
2020-09-11 18:07:25 +0300

    contactsPage

--- vet_clinic/js/main.js
+++ vet_clinic/js/main.js
@@ -12,3 +12,20 @@
         }
     }
 });
+
+var map;
+var marker;
+function initMap() {
+    var myLatLng = {lat: 47.107716, lng: 37.642368};
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        zoom: 14
+    });
+
+    marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        icon: 'img/contacts/point.png'
+    });
+}
