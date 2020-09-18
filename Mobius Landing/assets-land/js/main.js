b5e4bc544732ec49031812277f2230fa92a1061f
lleic12101 <50154894+lleic12101@users.noreply.github.com>
2020-09-18 19:44:45 +0300

    assetsFolder

---
+++ Mobius Landing/assets-land/js/main.js
@@ -0,0 +1,10 @@
+﻿//burger menu
+$("#burger1").click(function () {
+    $(".header__closebleMask").toggle();
+    $(".header__mobileToggleBlock").slideToggle();
+});
+$(".header__closebleMask").click(function () {
+    $("#burger1").prop("checked", false);
+    $(".header__mobileToggleBlock").slideUp();
+    $(".header__closebleMask").hide();
+});
