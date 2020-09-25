cf60e3f69f2345d9e22b1bd5fd15724ad63cb8a9
lleic12101 <50154894+lleic12101@users.noreply.github.com>
2020-09-25 14:53:35 +0300

    filterUpdate

--- viva_massage/js/main.js
+++ viva_massage/js/main.js
@@ -1,8 +1,31 @@
-﻿$('.section__servicesFilterItemTitle').click(function () {
+﻿if (sessionStorage.getItem('opened') != null) {
+    var openName = sessionStorage.getItem('opened');
+    for (var i = 0; i < $('.section__servicesFilterListLiItem p').length; i++) {
+        var text = $('.section__servicesFilterListLiItem p')[i];
+        if (text.innerHTML === openName) {
+            $(text).parent().parent().parent().slideDown();
+            $(text).parent().parent().parent().parent().parent().slideDown();
+            $(text).parent().parent().children('.section__servicesFilterList').slideDown();
+            $(text).parent().parent().parent().parent().children('.section__servicesFilterList').slideDown();
+            $(text).parent().parent().parent().parent().parent().parent().parent().parent().children('.section__servicesFilterList').slideDown();
+
+            $(text).parent().parent().parent().parent().children('.section__servicesFilterItemTitle').children('.section__servicesFilterSignBlock').toggleClass('section__servicesFilterSignBlock-active');
+            $(text).parent().parent().parent().parent().parent().parent().parent().parent().children('.section__servicesFilterItemTitle').children('.section__servicesFilterSignBlock').toggleClass('section__servicesFilterSignBlock-active');
+            $(text).parent().parent().parent().parent().children('.section__servicesFilterListLiItem').children('.section__servicesFilterSignBlock').toggleClass('section__servicesFilterSignBlock-active');
+            $(text).parent().parent().children('.section__servicesFilterListLiItem').children('.section__servicesFilterSignBlock').toggleClass('section__servicesFilterSignBlock-active');
+
+            $(text).parent().parent().children('.section__servicesFilterListLiItem').addClass('section__servicesFilterListLiItem-active');
+            $(text).parent().parent().parent().parent().children('.section__servicesFilterListLiItem').addClass('section__servicesFilterListLiItem-active');
+
+            i = $('.section__servicesFilterListLiItem p').length;
+        }
+    }
+}
+
+$('.section__servicesFilterItemTitle').click(function () {
     if ($(this).parent().children('.section__servicesFilterList').length > 0) {
         $(this).parent().children('.section__servicesFilterList').slideToggle();
         $(this).children('.section__servicesFilterSignBlock').toggleClass('section__servicesFilterSignBlock-active');
-
     }
 });
 $('.section__servicesFilterListLiItem').click(function () {
@@ -14,6 +37,10 @@ $('.section__servicesFilterListLiItem').click(function () {
     $(this).addClass('section__servicesFilterListLiItem-active');
     $(this).parent().parent().parent().children('.section__servicesFilterListLiItem').addClass('section__servicesFilterListLiItem-active');
     $(this).parent().parent().parent().parent().parent().children('.section__servicesFilterListLiItem').addClass('section__servicesFilterListLiItem-active');
+
+    var name = $(this).children('p').html();
+    sessionStorage.setItem('opened', name);
+    window.location.reload();
 });
 $('.section__serviceItemText-show').click(function () {
     $(this).parent().children(".section__serviceItemText-show").hide();
