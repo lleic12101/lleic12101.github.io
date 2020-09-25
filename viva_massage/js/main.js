fab932d163e8a47324e6aaab62160ce0cdf4fb8b
lleic12101 <50154894+lleic12101@users.noreply.github.com>
2020-09-25 13:10:21 +0300

    viva_massage

---
+++ viva_massage/js/main.js
@@ -0,0 +1,22 @@
+﻿$('.section__servicesFilterItemTitle').click(function () {
+    if ($(this).parent().children('.section__servicesFilterList').length > 0) {
+        $(this).parent().children('.section__servicesFilterList').slideToggle();
+        $(this).children('.section__servicesFilterSignBlock').toggleClass('section__servicesFilterSignBlock-active');
+
+    }
+});
+$('.section__servicesFilterListLiItem').click(function () {
+    if ($(this).parent().children('.section__servicesFilterList').length > 0) {
+        $(this).parent().children('.section__servicesFilterList').slideToggle();
+        $(this).children('.section__servicesFilterSignBlock').toggleClass('section__servicesFilterSignBlock-active');
+    }
+    $('.section__servicesFilterListLiItem').removeClass('section__servicesFilterListLiItem-active');
+    $(this).addClass('section__servicesFilterListLiItem-active');
+    $(this).parent().parent().parent().children('.section__servicesFilterListLiItem').addClass('section__servicesFilterListLiItem-active');
+    $(this).parent().parent().parent().parent().parent().children('.section__servicesFilterListLiItem').addClass('section__servicesFilterListLiItem-active');
+});
+$('.section__serviceItemText-show').click(function () {
+    $(this).parent().children(".section__serviceItemText-show").hide();
+    $(this).parent().children(".section__serviceItemText-short").hide();
+    $(this).parent().children(".section__serviceItemText-full").show();
+});
