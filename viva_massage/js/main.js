d99958159a4aef7b21bc9636bddd9438189f9ce8
lleic12101 <50154894+lleic12101@users.noreply.github.com>
2020-09-25 21:57:49 +0300

    adaptiva_apdate

--- viva_massage/js/main.js
+++ viva_massage/js/main.js
@@ -29,25 +29,74 @@ $('.section__serviceItemText-show').click(function () {
 
 //filter storage
 if (sessionStorage.getItem('opened') != null) {
-    var openName = sessionStorage.getItem('opened');
-    for (var i = 0; i < $('.section__servicesFilterListLiItem p').length; i++) {
-        var text = $('.section__servicesFilterListLiItem p')[i];
-        if (text.innerHTML === openName) {
-            $(text).parent().parent().parent().show();
-            $(text).parent().parent().parent().parent().parent().show();
-            $(text).parent().parent().children('.section__servicesFilterList').show();
-            $(text).parent().parent().parent().parent().children('.section__servicesFilterList').show();
-            $(text).parent().parent().parent().parent().parent().parent().parent().parent().children('.section__servicesFilterList').show();
+    var width = $(window).width();
+    if (width > 656) {
+        var openName = sessionStorage.getItem('opened');
+        for (var i = 0; i < $('.section__servicesFilterListLiItem p').length; i++) {
+            var text = $('.section__servicesFilterListLiItem p')[i];
+            if (text.innerHTML === openName) {
+                $(text).parent().parent().parent().show();
+                $(text).parent().parent().parent().parent().parent().show();
+                $(text).parent().parent().children('.section__servicesFilterList').show();
+                $(text).parent().parent().parent().parent().children('.section__servicesFilterList').show();
+                $(text).parent().parent().parent().parent().parent().parent().parent().parent().children('.section__servicesFilterList').show();
 
-            $(text).parent().parent().parent().parent().children('.section__servicesFilterItemTitle').children('.section__servicesFilterSignBlock').toggleClass('section__servicesFilterSignBlock-active');
-            $(text).parent().parent().parent().parent().parent().parent().parent().parent().children('.section__servicesFilterItemTitle').children('.section__servicesFilterSignBlock').toggleClass('section__servicesFilterSignBlock-active');
-            $(text).parent().parent().parent().parent().children('.section__servicesFilterListLiItem').children('.section__servicesFilterSignBlock').toggleClass('section__servicesFilterSignBlock-active');
-            $(text).parent().parent().children('.section__servicesFilterListLiItem').children('.section__servicesFilterSignBlock').toggleClass('section__servicesFilterSignBlock-active');
+                $(text).parent().parent().parent().parent().children('.section__servicesFilterItemTitle').children('.section__servicesFilterSignBlock').toggleClass('section__servicesFilterSignBlock-active');
+                $(text).parent().parent().parent().parent().parent().parent().parent().parent().children('.section__servicesFilterItemTitle').children('.section__servicesFilterSignBlock').toggleClass('section__servicesFilterSignBlock-active');
+                $(text).parent().parent().parent().parent().children('.section__servicesFilterListLiItem').children('.section__servicesFilterSignBlock').toggleClass('section__servicesFilterSignBlock-active');
+                $(text).parent().parent().children('.section__servicesFilterListLiItem').children('.section__servicesFilterSignBlock').toggleClass('section__servicesFilterSignBlock-active');
 
-            $(text).parent().parent().children('.section__servicesFilterListLiItem').addClass('section__servicesFilterListLiItem-active');
-            $(text).parent().parent().parent().parent().children('.section__servicesFilterListLiItem').addClass('section__servicesFilterListLiItem-active');
+                $(text).parent().parent().children('.section__servicesFilterListLiItem').addClass('section__servicesFilterListLiItem-active');
+                $(text).parent().parent().parent().parent().children('.section__servicesFilterListLiItem').addClass('section__servicesFilterListLiItem-active');
 
-            i = $('.section__servicesFilterListLiItem p').length;
+                i = $('.section__servicesFilterListLiItem p').length;
+            }
         }
     }
 }
+
+//show filter
+$('.header__navCityBtn').click(function () {
+    $('.section__servicesFilter').toggle();
+    $(this).toggleClass('header__navBtn-active');
+});
+
+//resize func
+$(window).on('resize', function () {
+    onResize();
+});
+onResize();
+
+function onResize() {
+    var width = $(window).width();
+    if (width <= 656) {
+        $('.header__navListBlock-gender').removeClass('header__navListBlock-opened');
+    } else $('.header__navListBlock-gender').addClass('header__navListBlock-opened');
+}
+
+//mobile menu
+var modal = document.querySelector('.modal');
+
+function toggleModal() {
+    modal.classList.toggle('show-modal');
+    $('html, body').toggleClass('body-modal');
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+$('.header__navBurgerLink').click(function () {
+    toggleModal();
+});
+$('.modal__contentHeaderClose').click(function () {
+    toggleModal();
+});
+window.addEventListener("click", windowOnClick);
+
+$('.modal__contentNavLiBtn').click(function () {
+    $(this).parent().children('.modal__contentNavListBlock').slideToggle();
+    $(this).toggleClass('modal__contentNavLiBtn-active');
+});
