8c07ad477ce9a4b28d9c98596684b75cded4d9de
lleic12101 <50154894+lleic12101@users.noreply.github.com>
2020-10-06 19:24:51 +0300

    newfilter

--- viva_massage/js/main.js
+++ viva_massage/js/main.js
@@ -29,7 +29,7 @@ $('.section__serviceItemText-show').click(function () {
 
 //show filter
 $('.header__navCityBtn').click(function () {
-    $('.section__servicesMobileFilter').toggle();
+    $('.section__servicesFilter').toggle();
     $(this).toggleClass('header__navBtn-active');
     if (!$(this).hasClass("header__navBtn-active")) {
         $(".header__navListBlock").removeClass("header__navListBlock-active");
@@ -182,260 +182,6 @@ var mySwiper = new Swiper('.swiper-container', {
         }
     },
 });
-
-//mobile filter
-function setCountries(iClass, names, links) {
-    var $listBlock = $(iClass).children('.section__servicesMobileFilterList');
-    var text = "";
-    for (var i = 0; i < names.length; i++) {
-        text += `
-        <li class="section__servicesMobileFilterListLi">
-                                        <a href="${links[i]}">${names[i]}</a>
-                                    </li>
-        `;
-    }
-    $($listBlock).html(text);
-}
-function getCountries(iClass, name) {
-    var strArr = [];
-    var $thatParent;
-
-    if (name == null) {
-        for (var i = 0; i < $('.section__servicesFilterItemTitle p').length; i++) {
-            var text = $('.section__servicesFilterItemTitle p')[i];
-            strArr.push(text.innerHTML);
-        }
-        return strArr;
-    }
-    if (iClass == "section__servicesMobileFilterItem-country") {
-        for (var i = 0; i < $('.section__servicesFilterItemTitle p').length; i++) {
-            var text = $('.section__servicesFilterItemTitle p')[i];
-            if (text.innerHTML == name) {
-                $thatParent = $(text).parent().parent().children('.section__servicesFilterList');
-            }
-        }
-    }
-    if (iClass == "section__servicesMobileFilterItem-city" ||
-        iClass == "section__servicesMobileFilterItem-part" ||
-        iClass == "section__servicesMobileFilterItem-area") {
-        for (var i = 0; i < $('.section__servicesFilterListLiItem p').length; i++) {
-            var text = $('.section__servicesFilterListLiItem p')[i];
-            if (text.innerHTML == name) {
-                $thatParent = $(text).parent().parent().children('.section__servicesFilterList');
-            }
-        }
-    }
-
-    $thatParent = $($thatParent).children('.section__servicesFilterListLi').children('.section__servicesFilterListLiItem').children('p');
-
-    for (var i = 0; i < $($thatParent).length; i++) {
-        var text = $($thatParent)[i];
-        strArr.push(text.innerHTML);
-    }
-    return strArr;
-}
-function getLinks(iClass, name) {
-    var strArr = [];
-    var $thatParent;
-
-    if (name == null) {
-        for (var i = 0; i < $('.section__servicesFilterItemTitle').length; i++) {
-            var text = $('.section__servicesFilterItemTitle')[i];
-            strArr.push(text.href);
-        }
-        return strArr;
-    }
-    if (iClass == "section__servicesMobileFilterItem-country") {
-        for (var i = 0; i < $('.section__servicesFilterItemTitle p').length; i++) {
-            var text = $('.section__servicesFilterItemTitle p')[i];
-            if (text.innerHTML == name) {
-                $thatParent = $(text).parent().parent().children('.section__servicesFilterList');
-            }
-        }
-    }
-    if (iClass == "section__servicesMobileFilterItem-city" ||
-        iClass == "section__servicesMobileFilterItem-part" ||
-        iClass == "section__servicesMobileFilterItem-area") {
-        for (var i = 0; i < $('.section__servicesFilterListLiItem p').length; i++) {
-            var text = $('.section__servicesFilterListLiItem p')[i];
-            if (text.innerHTML == name) {
-                $thatParent = $(text).parent().parent().children('.section__servicesFilterList');
-            }
-        }
-    }
-
-    $thatParent = $($thatParent).children('.section__servicesFilterListLi').children('.section__servicesFilterListLiItem');
-
-    for (var i = 0; i < $($thatParent).length; i++) {
-        var text = $($thatParent)[i];
-        strArr.push(text.href);
-    }
-    return strArr;
-}
-$('.section__servicesMobileFilterItemHeadline').click(function () {
-    $(this).parent().children('.section__servicesMobileFilterList').slideToggle();
-    $(this).toggleClass('section__servicesMobileFilterItemHeadline-active');
-});
-$('.section__servicesMobileFilterListLi').click(function () {
-    itemClick(this);
-});
-function itemClick(that, thatClass, thatName) {
-    var isNew = false;
-    var itemClass;
-    var itemName;
-    if (thatClass == null && thatName == null) {
-        itemClass = $(that).parent().parent().attr('class').split(/\s+/)[1];
-        itemName = $(that).children('a').html();
-    } else {
-        isNew = true;
-        itemClass = thatClass;
-        itemName = thatName;
-    }
-    var names = getCountries(itemClass, itemName);
-    var links = getLinks(itemClass, itemName);
-
-    sessionStorage.setItem(itemClass, itemName);
-
-    if (itemClass == "section__servicesMobileFilterItem-area" && isNew == false) {
-        $(that).parent().slideUp();
-        $(that).removeClass('section__servicesMobileFilterItemHeadline-active');
-        $(that).parent().parent().find('.section__servicesMobileFilterItemText').html(itemName);
-    } else {
-        if (itemName != null && itemName != "null") {
-            $('.section__servicesMobileFilterItem-area').find('.section__servicesMobileFilterItemText').html(itemName);
-        }
-    }
-
-
-    if (names.length == 0) {
-        if (isNew == false) {
-            window.location.reload();
-        }
-        return;
-    }
-
-    fieldsReset(itemClass);
-
-    if (isNew == false) {
-        $(that).parent().slideUp();
-        $(that).removeClass('section__servicesMobileFilterItemHeadline-active');
-        $(that).parent().parent().find('.section__servicesMobileFilterItemText').html(itemName);
-    } else {
-        if (itemName != null && itemName != "null") {
-            $('.' + itemClass).find('.section__servicesMobileFilterItemText').html(itemName);
-        }
-    }
-    if (itemClass == "section__servicesMobileFilterItem-country") {
-        $(".section__servicesMobileFilterItem-city").show();
-        setCountries('.section__servicesMobileFilterItem-city', names, links);
-    } else if (itemClass == "section__servicesMobileFilterItem-city") {
-        $(".section__servicesMobileFilterItem-part").show();
-        setCountries('.section__servicesMobileFilterItem-part', names, links);
-    } else if (itemClass == "section__servicesMobileFilterItem-part") {
-        $(".section__servicesMobileFilterItem-area").show();
-        setCountries('.section__servicesMobileFilterItem-area', names, links);
-    }
-
-    $('.section__servicesMobileFilterListLi').click(function () {
-        itemClick(this);
-    });
-
-    if (isNew == false) {
-        window.location.reload();
-    }
-}
-function fieldsReset(itemClass) {
-    if (itemClass == "section__servicesMobileFilterItem-country") {
-        $(".section__servicesMobileFilterItem-city").find('.section__servicesMobileFilterItemText').html("Select a city");
-        $(".section__servicesMobileFilterItem-part").find('.section__servicesMobileFilterItemText').html("Select part of the city");
-        $(".section__servicesMobileFilterItem-area").find('.section__servicesMobileFilterItemText').html("Select area");
-        sessionStorage.setItem('section__servicesMobileFilterItem-city', null);
-        sessionStorage.setItem('section__servicesMobileFilterItem-part', null);
-        sessionStorage.setItem('section__servicesMobileFilterItem-area', null);
-    } else if (itemClass == "section__servicesMobileFilterItem-city") {
-        $(".section__servicesMobileFilterItem-part").find('.section__servicesMobileFilterItemText').html("Select part of the city");
-        $(".section__servicesMobileFilterItem-area").find('.section__servicesMobileFilterItemText').html("Select area");
-        sessionStorage.setItem('section__servicesMobileFilterItem-part', null);
-        sessionStorage.setItem('section__servicesMobileFilterItem-area', null);
-    } else if (itemClass == "section__servicesMobileFilterItem-part") {
-        $(".section__servicesMobileFilterItem-area").find('.section__servicesMobileFilterItemText').html("Select area");
-        sessionStorage.setItem('section__servicesMobileFilterItem-area', null);
-    }
-}
-function initFilterMobile() {
-    var names = getCountries('..section__servicesMobileFilterItem-country');
-    var links = getLinks('..section__servicesMobileFilterItem-country');
-    setCountries('.section__servicesMobileFilterItem-country', names, links);
-
-    $('.section__servicesMobileFilterListLi').click(function () {
-        itemClick(this);
-    });
-}
-initFilterMobile();
-
-//filter storage
-if (sessionStorage.getItem('opened') != null) {
-    var width = $(window).width();
-    if (width > 656) {
-        var openName = sessionStorage.getItem('opened');
-        for (var i = 0; i < $('.section__servicesFilterListLiItem p').length; i++) {
-            var text = $('.section__servicesFilterListLiItem p')[i];
-            if (text.innerHTML === openName) {
-                $(text).parent().parent().parent().show();
-                $(text).parent().parent().parent().parent().parent().show();
-                $(text).parent().parent().children('.section__servicesFilterList').show();
-                $(text).parent().parent().parent().parent().children('.section__servicesFilterList').show();
-                $(text).parent().parent().parent().parent().parent().parent().parent().parent().children('.section__servicesFilterList').show();
-
-                $(text).parent().parent().parent().parent().children('.section__servicesFilterItemTitle').children('.section__servicesFilterSignBlock').toggleClass('section__servicesFilterSignBlock-active');
-                $(text).parent().parent().parent().parent().parent().parent().children('.section__servicesFilterItemTitle').children('.section__servicesFilterSignBlock').toggleClass('section__servicesFilterSignBlock-active');
-                $(text).parent().parent().parent().parent().parent().parent().parent().parent().children('.section__servicesFilterItemTitle').children('.section__servicesFilterSignBlock').toggleClass('section__servicesFilterSignBlock-active');
-                $(text).parent().parent().parent().parent().children('.section__servicesFilterListLiItem').children('.section__servicesFilterSignBlock').toggleClass('section__servicesFilterSignBlock-active');
-                $(text).parent().parent().children('.section__servicesFilterListLiItem').children('.section__servicesFilterSignBlock').toggleClass('section__servicesFilterSignBlock-active');
-
-                $(text).parent().parent().children('.section__servicesFilterListLiItem').addClass('section__servicesFilterListLiItem-active');
-                $(text).parent().parent().parent().parent().children('.section__servicesFilterListLiItem').addClass('section__servicesFilterListLiItem-active');
-                $(text).parent().parent().parent().parent().parent().parent().children('.section__servicesFilterListLiItem').addClass('section__servicesFilterListLiItem-active');
-
-                $(text).parent().parent().parent().parent().parent().parent().children('.section__servicesFilterListLiItem').children('.section__servicesFilterSignBlock').toggleClass('section__servicesFilterSignBlock-active');
-
-                i = $('.section__servicesFilterListLiItem p').length;
-            }
-        }
-    }
-}
-if ($(window).width() <= 656) {
-    headerResponsive();
-
-    var country = sessionStorage.getItem('section__servicesMobileFilterItem-country');
-    var city = sessionStorage.getItem('section__servicesMobileFilterItem-city');
-    var part = sessionStorage.getItem('section__servicesMobileFilterItem-part');
-    var area = sessionStorage.getItem('section__servicesMobileFilterItem-area');
-
-    // console.log(country);
-    // console.log(city);
-    // console.log(part);
-    // console.log(area);
-
-    if (country != null) {
-        $('.section__servicesMobileFilter').show();
-        $('.header__navCityBtn').addClass("header__navBtn-active");
-        $('.header__navCityBtn').children(".header__navIconClose").show();
-        $('.header__navCityBtn').children(".header__navBtnIcon").hide();
-        itemClick(null, 'section__servicesMobileFilterItem-country', country);
-    }
-    if (city != null) {
-        itemClick(null, 'section__servicesMobileFilterItem-city', city);
-    }
-    if (part != null) {
-        itemClick(null, 'section__servicesMobileFilterItem-part', part);
-    }
-    if (area != null && area != "null") {
-        $('.section__servicesMobileFilter').hide();
-        $('.header__navCityBtn').removeClass("header__navBtn-active");
-        itemClick(null, 'section__servicesMobileFilterItem-area', area);
-    }
-}
 
 //textarea checker
 textareaInit();
@@ -788,7 +534,7 @@ function headerResponsive() {
     $(".header__navList").removeClass("hover");
     $(".header__navLink").removeClass("hover");
     $('.header__navListBlock').click(function () {
-        $('.section__servicesMobileFilter').hide();
+        $('.section__servicesFilter').hide();
         $('.header__navCityBtn').removeClass("header__navBtn-active");
         $(".header__navIconClose").hide();
         $(".header__navBtnIcon").show();
@@ -807,4 +553,7 @@ function headerResponsive() {
         e.preventDefault();
         e.stopPropagation();
     });
+}
+if ($(window).width() <= 656) {
+    headerResponsive();
 }
