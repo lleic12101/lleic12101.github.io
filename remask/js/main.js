f0ca14d551ad2e51d3a72de3ad9d11cc5513828c
Ilya Bindas <50154894+lleic12101@users.noreply.github.com>
2020-11-09 22:00:53 +0200

    cardjs

--- remask/js/main.js
+++ remask/js/main.js
@@ -392,6 +392,8 @@ if ($('*').is('.main__bagCartItemDelete')) {
     $(".main__bagCartItemDelete").click(function () {
         $(this).parent().remove();
     });
+}
+if ($('*').is('.main__bagCartItemCounterMinusBlock')) {
     $(".main__bagCartItemCounterMinusBlock").click(function () {
         var num = $(this).parent().parent().find(".main__bagCartItemCounterNum").html();
         if (num > 1) {
@@ -462,4 +464,59 @@ function checkFilterStatus() {
     for (var i = 0; i < $btns.length; i++) if ($($btns[i]).hasClass("main__productsItemsSortBtn-active")) return;
     if ($(".main__productsItemsSortByLink").html().trim() !== "По популярности") return;
     $(".main__productsHeaderSort").removeClass("main__productsHeaderSort-active");
+}
+
+//card size
+if ($('*').is('.main__cardContentSizeBtn')) {
+    $(".main__cardContentSizeBtn").click(function () {
+        $(".main__cardContentSizeBtn").removeClass("main__cardContentSizeBtn-active");
+        $(this).addClass("main__cardContentSizeBtn-active");
+    });
+}
+if ($('*').is('.main__cardImgScrolling')) {
+    (function () {
+        var a = document.querySelector('.main__cardImgScrolling'), b = null, P = 170;  // если ноль заменить на число, то блок будет прилипать до того, как верхний край окна браузера дойдёт до верхнего края элемента. Может быть отрицательным числом
+        window.addEventListener('scroll', Ascroll, false);
+        document.body.addEventListener('scroll', Ascroll, false);
+        function Ascroll() {
+            if (window.innerWidth > 1261) {
+                if (b == null) {
+                    var Sa = getComputedStyle(a, ''), s = '';
+                    for (var i = 0; i < Sa.length; i++) {
+                        if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
+                            s += Sa[i] + ': ' + Sa.getPropertyValue(Sa[i]) + '; '
+                        }
+                    }
+                    b = document.createElement('div');
+                    b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
+                    a.insertBefore(b, a.firstChild);
+                    var l = a.childNodes.length;
+                    for (var i = 1; i < l; i++) {
+                        b.appendChild(a.childNodes[1]);
+                    }
+                    a.style.height = b.getBoundingClientRect().height + 'px';
+                    a.style.padding = '0';
+                    a.style.margin = '0';
+                    a.style.border = '0';
+                }
+                var Ra = a.getBoundingClientRect(),
+                    R = Math.round(Ra.top + b.getBoundingClientRect().height - document.querySelector('.main__bagSpecialBlock-with').getBoundingClientRect().top + 0);  // селектор блока, при достижении верхнего края которого нужно открепить прилипающий элемент;  Math.round() только для IE; если ноль заменить на число, то блок будет прилипать до того, как нижний край элемента дойдёт до футера
+                if ((Ra.top - P) <= 0) {
+                    if ((Ra.top - P) <= R) {
+                        b.className = 'stop';
+                        b.style.top = -R + 'px';
+                    } else {
+                        b.className = 'sticky';
+                        b.style.top = P + 'px';
+                    }
+                } else {
+                    b.className = '';
+                    b.style.top = '';
+                }
+                window.addEventListener('resize', function () {
+                    a.children[0].style.width = getComputedStyle(a, '').width
+                }, false);
+            }
+        }
+    })()
 }
