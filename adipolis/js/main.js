7fbc8e32321e04be4a539fd0c87790fb249edd39
Ilya Bindas <50154894+lleic12101@users.noreply.github.com>
2020-11-18 00:27:03 +0200

    pc

---
+++ adipolis/js/main.js
@@ -0,0 +1,10 @@
+﻿$('.main__footerContentBtn').click(function () {
+    $('.main__footerContentBtn').removeClass('main__footerContentBtn-active');
+    $(this).addClass('main__footerContentBtn-active');
+
+    var id = $(this).attr("id");
+    id = id.substr(0, id.length - 3);
+
+    $('.main__footerContentTab').removeClass("main__footerContentTab-active");
+    $("#" + id + "tab").addClass("main__footerContentTab-active");
+});
