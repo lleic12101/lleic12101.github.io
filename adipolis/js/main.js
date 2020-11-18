d8327196ebb2e2f42336dac612b63eca4737cfea
Ilya Bindas <50154894+lleic12101@users.noreply.github.com>
2020-11-18 19:54:19 +0200

    adaptive

--- adipolis/js/main.js
+++ adipolis/js/main.js
@@ -1,4 +1,32 @@
-﻿$('.main__footerContentBtn').click(function () {
+﻿//burger menu
+$('.checkbox1').click(function () {
+    $('.header__closebleMask').toggleClass("header__closebleMask-active");
+    $('.header__navToggleBlock-mobile').slideToggle();
+});
+$('.header__closebleMask').click(function () {
+    $('.header__closebleMask').removeClass("header__closebleMask-active");
+    $('.header__navToggleBlock-mobile').slideUp();
+    $('.checkbox1').prop("checked", false);
+});
+
+//scroll to
+(function () {
+    link = $('.js-scroll-link');
+    link.on('click', function () {
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        if ($(window).width() < 768) {
+            top -= 60;
+            $('.header__closebleMask').removeClass("header__closebleMask-active");
+            $('.header__navToggleBlock-mobile').slideUp();
+            $('.checkbox1').prop("checked", false);
+        }
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+})();
+
+//tabs
+$('.main__footerContentBtn').click(function () {
     $('.main__footerContentBtn').removeClass('main__footerContentBtn-active');
     $(this).addClass('main__footerContentBtn-active');
 
