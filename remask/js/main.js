d843cb11f598f86760f39af7ab3e1268800e7ad9
Ilya Bindas <50154894+lleic12101@users.noreply.github.com>
2020-11-28 22:34:45 +0200

    upd

--- remask/js/main.js
+++ remask/js/main.js
@@ -647,4 +647,168 @@ if ($('*').is('video')) {
     }
 }
 
+//autoscroll
+(function (){
+    link = $('.js-scroll-link');
+    link.on('click', function () {
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
+})();
+
+//country api
+var log = function(e){//облегчаем себе жизнь
+    return(console.log(e));
+}
+var autocomplete_service = new google.maps.places.AutocompleteService();//подключаем гуглосервис
+var counrty_val = ' ';//это сюда будем писать значения наших местоположений
+var region_val = ' ';
+var city_val = ' ';
+var district_val = ' ';
+var country_list = [];//массивы для всплывающих списков
+var region_list = [];
+var city_list = [];
+var district_list = [];
+
+$('#country').keyup(function(){
+    country_val = $('#country').val();//получаем буквы из нашего инпута для страны
+
+    if(country_val){
+        var request = {
+            input: country_val,
+        };
+        //log(request);
+        autocomplete_service.getPlacePredictions(request, google_addresses_callback);//делаем запрос в гугл
+        function google_addresses_callback(results, status) {
+            console.log(results);
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+                var counter = 0;
+                $.each(results, function (index, result) {
+                    log(result);
+                    function in_array(value, array) {
+                        for (var i = 0; i < array.length; i++) {
+                            if (array[i] == value){
+                                country_list[counter] = result.description;//заполняем результатами из гугла список подсказок во всплывашке
+                                counter++;
+                                return true;
+                            }
+                        }
+                        return false;
+                    }
+                    if (in_array("country", result.types)) {
+                        log(result);
+                    }
+                });
+            }
+            else{
+                log('no result');
+            }
+        }
+        $("#country").autocomplete({//инициализируем Jquery UI Autocomplete
+            source: country_list,
+            focus: displayItem,
+            select: displayItem,
+            change: displayItem
+        });
+        function displayItem(event, ui) {//если автоподстановка произошла, забираем новое значение
+            if(ui.item){
+                country_val = ui.item.label;
+            }
+        }
+    }
+});
+//в принципе дальше по аналогии с небольшими изменениями
+$('#region').keyup(function(){
+    region_val = country_val+' '+$('#region').val();//дописываем к значению инпута, значение предведущего инпута
+    var request = {
+        input: region_val,
+    };
+    //log(request);
+    autocomplete_service.getPlacePredictions(request, google_addresses_callback);
+    function google_addresses_callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            var counter = 0;
+            $.each(results, function (index, result) {
+                //log(result);
+                function in_array(value, array) {
+                    for (var i = 0; i < array.length; i++) {
+                        if (array[i] == value){
+                            var formated = result.description;
+                            formated = formated.split(',');//отсекаем значение предведущего инпута. в подсказке оно нам не нужно
+                            region_list[counter] = formated[0];
+                            counter++;
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+                if (in_array("administrative_area_level_1", result.types)) {
+                    //log(result);
+                }
+            });
+        }
+        else{
+            log('no result')
+        }
+    }
+    $( "#region" ).autocomplete({
+        source: region_list,
+        focus: displayItem,
+        select: displayItem,
+        change: displayItem
+    });
+    function displayItem(event, ui) {
+        if(ui.item){
+            region_val = ui.item.label;
+        }
+    }
+});
+$('#city').keyup(function(){
+    city_val = region_val+' '+$('#city').val();
+    var request = {
+        input: city_val,
+    };
+    //log(request);
+    autocomplete_service.getPlacePredictions(request, google_addresses_callback);
+    function google_addresses_callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            var counter = 0;
+            $.each(results, function (index, result) {
+                //log(result);
+                function in_array(value, array) {
+                    for (var i = 0; i < array.length; i++) {
+                        if (array[i] == value){
+                            var formated = result.description;
+                            formated = formated.split(',');
+                            city_list[counter] = formated[0];
+                            counter++;
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+                if (in_array("locality", result.types)) {
+                    //log(result);
+                }
+            });
+        }
+        else{
+            log('no result')
+        }
+    }
+    $( "#city" ).autocomplete({
+        source: city_list,
+        focus: displayItem,
+        select: displayItem,
+        change: displayItem
+    });
+    function displayItem(event, ui) {
+        if(ui.item){
+            city_val = ui.item.label;
+        }
+    }
+});
+
+
 
