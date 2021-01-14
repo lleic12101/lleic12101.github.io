0bc943a437edb9c3cb4048d6a76faa1abb461cdd
Ilya Bindas <50154894+lleic12101@users.noreply.github.com>
2021-01-14 20:19:44 +0200

    Investec

---
+++ Investec/js/main.js
@@ -0,0 +1,15 @@
+﻿var swiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 1,
+            centeredSlides: true,
+            loop: true,
+        },
+        960: {
+            slidesPerView: 3,
+        },
+    }
+});
