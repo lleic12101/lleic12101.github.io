d1a2363065a2c8a8db6dd7c1ab70aa62795cb15c
Ilya Bindas <50154894+lleic12101@users.noreply.github.com>
2021-01-16 00:06:56 +0200

    contacts

--- Investec/js/main.js
+++ Investec/js/main.js
@@ -57,3 +57,113 @@ $(".main__questionsItemHeader").click(function () {
     $(this).parent().toggleClass("main__questionsItem-active");
     $(this).parent().find(".main__questionsItemAnswer").slideToggle();
 });
+
+//map
+if ($('*').is('.main__contactsContentMapBlock')) {
+    var map;
+    var marker;
+
+    function initMap() {
+        var myLatLng = {lat: 55.776583, lng: 37.500132};
+
+        map = new google.maps.Map(document.getElementById("map"), {
+            center: myLatLng,
+            zoom: 14,
+            styles: [
+                {elementType: "geometry", stylers: [{color: "#242f3e"}]},
+                {elementType: "labels.text.stroke", stylers: [{color: "#242f3e"}]},
+                {elementType: "labels.text.fill", stylers: [{color: "#746855"}]},
+                {
+                    featureType: "administrative.locality",
+                    elementType: "labels.text.fill",
+                    stylers: [{color: "#d59563"}],
+                },
+                {
+                    featureType: "poi",
+                    elementType: "labels.text.fill",
+                    stylers: [{color: "#d59563"}],
+                },
+                {
+                    featureType: "poi.park",
+                    elementType: "geometry",
+                    stylers: [{color: "#263c3f"}],
+                },
+                {
+                    featureType: "poi.park",
+                    elementType: "labels.text.fill",
+                    stylers: [{color: "#6b9a76"}],
+                },
+                {
+                    featureType: "road",
+                    elementType: "geometry",
+                    stylers: [{color: "#38414e"}],
+                },
+                {
+                    featureType: "road",
+                    elementType: "geometry.stroke",
+                    stylers: [{color: "#212a37"}],
+                },
+                {
+                    featureType: "road",
+                    elementType: "labels.text.fill",
+                    stylers: [{color: "#9ca5b3"}],
+                },
+                {
+                    featureType: "road.highway",
+                    elementType: "geometry",
+                    stylers: [{color: "#746855"}],
+                },
+                {
+                    featureType: "road.highway",
+                    elementType: "geometry.stroke",
+                    stylers: [{color: "#1f2835"}],
+                },
+                {
+                    featureType: "road.highway",
+                    elementType: "labels.text.fill",
+                    stylers: [{color: "#f3d19c"}],
+                },
+                {
+                    featureType: "transit",
+                    elementType: "geometry",
+                    stylers: [{color: "#2f3948"}],
+                },
+                {
+                    featureType: "transit.station",
+                    elementType: "labels.text.fill",
+                    stylers: [{color: "#d59563"}],
+                },
+                {
+                    featureType: "water",
+                    elementType: "geometry",
+                    stylers: [{color: "#17263c"}],
+                },
+                {
+                    featureType: "water",
+                    elementType: "labels.text.fill",
+                    stylers: [{color: "#515c6d"}],
+                },
+                {
+                    featureType: "water",
+                    elementType: "labels.text.stroke",
+                    stylers: [{color: "#17263c"}],
+                },
+            ],
+        });
+
+        marker = new google.maps.Marker({
+            position: myLatLng,
+            title: "Место на карте",
+            map: map,
+            icon: "img/icons/map-point.png",
+        });
+    }
+}
+
+//input style
+if ($('*').is(".main__nameAndInputFooterInput")) {
+    $(".main__nameAndInputFooterInput").focus(function () {
+        $(".main__nameAndInputFooterInput").parent().parent().removeClass("main__nameAndInput-active");
+        $(this).parent().parent().addClass("main__nameAndInput-active");
+    });
+}
