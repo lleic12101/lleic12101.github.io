2af15ecef90cd715b0c8325aab6411b9bc0ec3c7
Illia Byndas <50154894+lleic12101@users.noreply.github.com>
2021-02-04 18:46:19 +0200

    index

---
+++ WorkForYou/js/main.js
@@ -0,0 +1,24 @@
+﻿//plans slider
+if ($('*').is('.owl-carousel')) {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        responsiveClass: true,
+        dots: true,
+        dotsEach: true,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+                dots: true,
+            },
+            1024: {
+                items: 3,
+                loop: false,
+            }
+        }
+    });
+}
