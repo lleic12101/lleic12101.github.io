1acd4d92846778333ec944b3c2e167e4e3aa99c3
Illia Byndas <50154894+lleic12101@users.noreply.github.com>
2021-02-06 12:15:35 +0200

    faq

--- WorkForYou/js/main.js
+++ WorkForYou/js/main.js
@@ -1,4 +1,38 @@
-﻿//plans slider
+﻿//resize func
+$(window).on('resize', function () {
+    resizeEmptySpace();
+
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+resizeEmptySpace();
+
+function resizeEmptySpace() {
+    // if ($('*').is('.main__certificate')) return;
+
+    $(".main").css("height", "auto");
+
+    var header = $(".header").innerHeight();
+    var footer = $(".footer").innerHeight();
+    var windowHeight = $(window).height();
+    var main = $(".main").innerHeight();
+
+    if ($(window).width() >= 1535 && $(window).width() <= 1537) {
+        windowHeight += (windowHeight / 100) * 25;
+    }
+    if ($(window).width() >= 1439 && $(window).width() <= 1441) {
+        windowHeight += (windowHeight / 100) * 25;
+    }
+    if ((header + footer + main) < windowHeight) {
+        $(".main").css("height", (windowHeight - (header + footer)));
+    } else {
+        $(".main").css("height", "auto");
+    }
+}
+
+//plans slider
 if ($('*').is('.owl-carousel')) {
     $('.owl-carousel').owlCarousel({
         loop: true,
@@ -34,4 +68,31 @@ $('.header__closeableMask').click(function () {
     $('.header__closeableMask').removeClass("header__closeableMask-active");
     $('.header__navWrapper').removeClass("header__navWrapper-active");
     $("html, body").removeClass("body-fixed");
+});
+
+//tabs
+$('.main__faqBtn').click(function () {
+    $('.main__faqBtn').removeClass('main__faqBtn-active');
+    $(this).addClass('main__faqBtn-active');
+
+    var id = $(this).attr("id");
+    id = id.substr(0, id.length - 3);
+
+    $('.main__faqTab').removeClass("main__faqTab-active");
+    $("#" + id).addClass("main__faqTab-active");
+});
+
+//faq
+$(".main__faqTabItemHeader").click(function () {
+    if ($(this).hasClass("main__faqTabItemHeader-active")) {
+        $(".main__faqTabItemHeader").removeClass("main__faqTabItemHeader-active");
+        $(".main__faqTabItemText").slideUp();
+    } else {
+        $(".main__faqTabItemHeader").removeClass("main__faqTabItemHeader-active");
+        $(".main__faqTabItemText").slideUp();
+        $(this).parent().find(".main__faqTabItemText").slideDown();
+        $(this).addClass("main__faqTabItemHeader-active");
+
+        resizeEmptySpace();
+    }
 });
