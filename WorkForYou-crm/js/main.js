c6520aa8995d54f5ee81e91d29650bd72d093336
Illia Byndas <50154894+lleic12101@users.noreply.github.com>
2021-02-07 20:56:08 +0200

    index

---
+++ WorkForYou-crm/js/main.js
@@ -0,0 +1,27 @@
+﻿//copy to clipboard
+$('.main__referralBtn').click(function () {
+    copyToClipboard();
+});
+$('.main__referralBtn-mobile').click(function () {
+    copyToClipboard();
+});
+function copyToClipboard(){
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($('.main__referralInput').val()).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
+
+//burger menu
+$('.ham').click(function () {
+    $("html, body").toggleClass("body-fixed");
+    $('.header__navWrapper').toggleClass("header__navWrapper-active");
+    $('.header__closeableMask').addClass("header__closeableMask-active");
+});
+$('.header__closeableMask').click(function () {
+    $('.ham').removeClass("active");
+    $('.header__closeableMask').removeClass("header__closeableMask-active");
+    $('.header__navWrapper').removeClass("header__navWrapper-active");
+    $("html, body").removeClass("body-fixed");
+});
