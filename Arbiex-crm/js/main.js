b5aacdb047e7b8eb2b680d6c0c2636a9e611c670
Illia Byndas <50154894+lleic12101@users.noreply.github.com>
2021-02-23 17:48:15 +0200

    crm

---
+++ Arbiex-crm/js/main.js
@@ -0,0 +1,12 @@
+﻿//burger menu
+$('.ham').click(function () {
+    $("html, body").toggleClass("body-fixed");
+    $('.aside__navWrapper').toggleClass("aside__navWrapper-active");
+    $('.aside__closeableMask').addClass("aside__closeableMask-active");
+});
+$('.aside__closeableMask').click(function () {
+    $('.ham').removeClass("active");
+    $('.aside__closeableMask').removeClass("aside__closeableMask-active");
+    $('.aside__navWrapper').removeClass("aside__navWrapper-active");
+    $("html, body").removeClass("body-fixed");
+});
