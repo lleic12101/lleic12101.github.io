49d69fc9f86aba303b7d21574e29c2d7f1cbafb3
Illia Byndas <50154894+lleic12101@users.noreply.github.com>
2021-02-24 16:21:56 +0200

    addFunds

--- Arbiex-crm/js/main.js
+++ Arbiex-crm/js/main.js
@@ -1,4 +1,34 @@
-﻿//burger menu
+﻿//resize func
+$(window).on('resize', function () {
+    resizeEmptySpace();
+
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+resizeEmptySpace();
+
+function resizeEmptySpace() {
+    // if ($('*').is('.main__certificate')) return;
+
+    $(".main").css("height", "auto");
+
+    var header = $(".aside").innerHeight();
+    var footer = $(".footer").innerHeight();
+    var windowHeight = $('body').height();
+    var main = $(".main").innerHeight();
+
+    if ((header + footer + main) < windowHeight) {
+        if ($(window).width() < 1400) {
+            $(".main").css("height", (windowHeight - (header + footer)) + 70);
+        }
+    } else {
+        $(".main").css("height", "auto");
+    }
+}
+
+//burger menu
 $('.ham').click(function () {
     $("html, body").toggleClass("body-fixed");
     $('.aside__navWrapper').toggleClass("aside__navWrapper-active");
@@ -9,4 +39,10 @@ $('.aside__closeableMask').click(function () {
     $('.aside__closeableMask').removeClass("aside__closeableMask-active");
     $('.aside__navWrapper').removeClass("aside__navWrapper-active");
     $("html, body").removeClass("body-fixed");
+});
+
+//addFunds
+$('.main__addFundsItem').click(function () {
+    $('.main__addFundsItem').removeClass('main__addFundsItem-active');
+    $(this).addClass('main__addFundsItem-active');
 });
