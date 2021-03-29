19e65b63707e2d6f8487c13e1e809a150c3b7225
Illia Byndas <50154894+lleic12101@users.noreply.github.com>
2021-03-29 15:18:27 +0300

    menu

--- Traddex/js/main.js
+++ Traddex/js/main.js
@@ -28,13 +28,13 @@ function resizeEmptySpace() {
 }
 
 //burger menu
-$('.ham').click(function () {
+$('.checkbox1').click(function () {
     $("html, body").toggleClass("body-fixed");
     $('.header__navWrapper').toggleClass("header__navWrapper-active");
     $('.header__closeableMask').addClass("header__closeableMask-active");
 });
 $('.header__closeableMask').click(function () {
-    $('.ham').removeClass("active");
+    $('.checkbox1').prop("checked", false);
     $('.header__closeableMask').removeClass("header__closeableMask-active");
     $('.header__navWrapper').removeClass("header__navWrapper-active");
     $("html, body").removeClass("body-fixed");
