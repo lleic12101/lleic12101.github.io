71f34262a2323c1d2a02dad3961f5abcd3a805df
Illia Byndas <50154894+lleic12101@users.noreply.github.com>
2021-04-10 17:51:55 +0300

    ok dream

---
+++ okdream/js/main.js
@@ -0,0 +1,34 @@
+﻿//scrollup btn
+$('.scrollup').click(function () {
+    $("html, body").animate({
+        scrollTop: 0
+    }, 1000);
+})
+function checkScroll() {
+    if ($(this).scrollTop() > 200) {
+        $('.scrollup').addClass('scrollup-active');
+    } else {
+        $('.scrollup').removeClass('scrollup-active');
+    }
+}
+
+//sticky header
+// var c, currentScrollTop = 0, navbar = $("header");
+$(window).scroll(function () {
+    // var a = $(window).scrollTop();
+    // var b = 60;
+    //
+    // currentScrollTop = a;
+    //
+    // if (c < currentScrollTop && a > b + b && !$("body").hasClass("body-fixed")) {
+    //     navbar.addClass("scrollUp");
+    //     $("header .navbar-collapsed-nav").slideUp(300);
+    // } else if (c > currentScrollTop && !(a <= b)) {
+    //     navbar.removeClass("scrollUp")
+    // }
+    //
+    // c = currentScrollTop;
+
+    checkScroll();
+    // checkScrollBuy();
+});
