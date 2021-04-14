a43102aa991fd22138c56a7792b86170556856c9
Illia Byndas <50154894+lleic12101@users.noreply.github.com>
2021-04-15 02:02:45 +0300

    popup

--- okdream-crm/js/main.js
+++ okdream-crm/js/main.js
@@ -53,3 +53,31 @@ $('.header-currency__close').click(function (e) {
     //     $('.header-currency__dropdown').css({"display": ""});
     // }, 300);
 });
+
+//modal
+$('.modal__close, .modal__button').click(function () {
+    toggleModal();
+});
+function toggleModal(id) {
+    if (id == null) {
+        $('.modal').removeClass('show-modal');
+    } else {
+        var modal = document.querySelector('#' + id);
+        modal.classList.add('show-modal');
+    }
+}
+function windowOnClick(event) {
+    var modal = document.querySelectorAll('.modal');
+    for (var i = 0; i < modal.length; i++) {
+        if (event.target === modal[i]) {
+            toggleModal();
+        }
+    }
+}
+window.addEventListener("click", windowOnClick);
+// toggleModal('modal5');
+
+//modal event
+$('.button-modal2').click(function () {
+    toggleModal('modal2');
+});
