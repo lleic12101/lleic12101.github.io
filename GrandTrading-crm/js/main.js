007cec77e7eedcb7383e58926cfb4ceda592ca8d
Illia Byndas <50154894+lleic12101@users.noreply.github.com>
2021-04-28 22:01:58 +0300

    index

---
+++ GrandTrading-crm/js/main.js
@@ -0,0 +1,33 @@
+﻿//resize func
+$(window).on('resize', function () {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+//burger menu
+$('.ham').click(function () {
+    $("html, body").toggleClass("body-fixed");
+    $('.header-nav__wrapper-mobile').toggleClass("header-nav__wrapper-active");
+    $('.header-mask').toggleClass("header-mask__active");
+});
+$('.header-mask').click(function () {
+    $('.ham').removeClass("active");
+    $('.header-mask').removeClass("header-mask__active");
+    $('.header-nav__wrapper').removeClass("header-nav__wrapper-active");
+    $("html, body").removeClass("body-fixed");
+});
+
+//copy to clipboard
+$('.main-link__button').click(function () {
+    copyToClipboard();
+});
+function copyToClipboard() {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($('.main-link__input').val()).select();
+    document.execCommand("copy");
+    $temp.remove();
+
+    $(".main-link__copied").text("copied");
+}
