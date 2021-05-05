f47b972a92b706f78d3ab922195770e1e5172db7
Illia Byndas <50154894+lleic12101@users.noreply.github.com>
2021-05-05 18:08:40 +0300

    kairos

---
+++ Kairos/js/main.js
@@ -0,0 +1,18 @@
+﻿//resize func
+$(window).on('resize', function () {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+//burger menu
+$('.header-burger').click(function () {
+    $("html, body").toggleClass("body-fixed");
+    $('.header-nav__wrapper').toggleClass("header-nav__wrapper-active");
+    $('.header-mask').toggleClass("header-mask__active");
+});
+$('.header-mask').click(function () {
+    $('.header-nav__wrapper').removeClass("header-nav__wrapper-active");
+    $('.header-mask').removeClass("header-mask__active");
+    $("html, body").removeClass("body-fixed");
+});
