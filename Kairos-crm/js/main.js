a275c9c0db1b281e4645531ec111ea689e3cf4a7
Illia Byndas <50154894+lleic12101@users.noreply.github.com>
2021-05-07 22:19:38 +0300

    index

--- Kairos-crm/js/main.js
+++ Kairos-crm/js/main.js
@@ -16,3 +16,17 @@ $('.main-systems__item').click(function () {
     $('.main-systems__item').removeClass('selected');
     $(this).addClass('selected');
 });
+
+//copy to clipboard
+$('.main-input__button-copy').click(function () {
+    copyToClipboard();
+});
+function copyToClipboard() {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($('.main-input__input').val()).select();
+    document.execCommand("copy");
+    $temp.remove();
+
+    $(".main-input__copied").text("copied");
+}
