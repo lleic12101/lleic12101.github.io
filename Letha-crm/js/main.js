ddd8cb019af46e6324b53e7e2becd47e1dd3c2b0
Illia Byndas <50154894+lleic12101@users.noreply.github.com>
2021-06-12 20:34:04 +0300

    data

--- Letha-crm/js/main.js
+++ Letha-crm/js/main.js
@@ -65,17 +65,71 @@ $('.main-deposit__item').click(function () {
     $(this).addClass('main-deposit__item-active');
 });
 
-
 //copy to clipboard
-$('.main-header__copy').click(function () {
+$('.main-team__clipboardButton').click(function () {
     copyToClipboard();
 });
 function copyToClipboard() {
     var $temp = $("<input>");
     $("body").append($temp);
-    $temp.val($('.main__nameAndInputFooterInput').val()).select();
+    $temp.val($('.main-team__clipboardInput').val()).select();
     document.execCommand("copy");
     $temp.remove();
 
     $(".main__nameAndInputCopied").text("copied");
 }
+
+//team buttons
+$('.main-team__button').click(function () {
+    $('.main-team__button').removeClass('main-team__button-active');
+    $(this).addClass('main-team__button-active');
+});
+
+//team menus
+$('.main-table__itemMenu .main-table__itemText, .main-team__sum').click(function () {
+    var open = true;
+
+    if ($(this).parent().hasClass('main-table__itemMenu-active')) open = false;
+
+    $('.main-team__menu').hide();
+    $('.main-table__itemMenu, .main-team__textBlock').removeClass('main-table__itemMenu-active');
+
+    if (open) {
+        $(this).parent().find('.main-team__menu').show();
+        $(this).parent().addClass('main-table__itemMenu-active');
+    }
+});
+
+//settings tabs
+$('.main-settings__button').click(function () {
+    $('.main-settings__button').removeClass('main-settings__button-active');
+    $(this).addClass('main-settings__button-active');
+
+    var id = $(this).attr("id");
+    id = id.substr(0, id.length - 3);
+
+    $('.main-settings__tab').removeClass("main-settings__tab-active");
+    $("#" + id).addClass("main-settings__tab-active");
+});
+
+//modal
+function toggleModal(id) {
+    if (id == null) {
+        $('.modal').removeClass('show-modal');
+    } else {
+        var modal = document.querySelector('#' + id);
+        modal.classList.add('show-modal');
+    }
+}
+function windowOnClick(event) {
+    var modal = document.querySelectorAll('.modal');
+    for (var i = 0; i < modal.length; i++) {
+        if (event.target === modal[i]) {
+            toggleModal();
+        }
+    }
+}
+window.addEventListener("click", windowOnClick);
+
+toggleModal("modal1");
+toggleModal("modal2");
