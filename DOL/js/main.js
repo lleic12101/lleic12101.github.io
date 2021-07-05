a0b4ab8a52f30ac4ecb5eef16bc0a0e252f368d8
Illia Byndas <50154894+lleic12101@users.noreply.github.com>
2021-07-05 20:16:58 +0300

    DOL

---
+++ DOL/js/main.js
@@ -0,0 +1,11 @@
+﻿//resize func
+$(window).on('resize', function () {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+//conversation video change
+$('#conversation-change').click(function () {
+    $('.main-conversation').toggleClass('main-conversation-video');
+});
