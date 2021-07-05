afe76034b10cb608b799e1e04c53130cdd9e4f46
Illia Byndas <50154894+lleic12101@users.noreply.github.com>
2021-07-05 23:40:49 +0300

    chat

--- DOL/js/main.js
+++ DOL/js/main.js
@@ -5,7 +5,11 @@ $(window).on('resize', function () {
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
 
+//beautiful scroll
+$('.main-chat__items').overlayScrollbars({});
+
 //conversation video change
 $('#conversation-change').click(function () {
     $('.main-conversation').toggleClass('main-conversation-video');
+    $('.main-chat__items').toggleClass('main-chat__items-short');
 });
