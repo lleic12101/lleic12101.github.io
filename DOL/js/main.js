9ac437ec21f73f9304e4ec9263e7ffce8c914c90
Illia Byndas <50154894+lleic12101@users.noreply.github.com>
2021-07-07 01:11:53 +0300

    updated

--- DOL/js/main.js
+++ DOL/js/main.js
@@ -1,15 +1,23 @@
-﻿//resize func
-$(window).on('resize', function () {
-    // We execute the same script as before
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
-
-//beautiful scroll
-$('.main-chat__items').overlayScrollbars({});
+﻿var doc = document;
 
 //conversation video change
-$('#conversation-change').click(function () {
-    $('.main-conversation').toggleClass('main-conversation-video');
-    $('.main-chat__items').toggleClass('main-chat__items-short');
-});
+doc.getElementById("conversation-change").onclick = function () {
+    doc.querySelector('.main-conversation').classList.toggle('main-conversation-video');
+    doc.querySelector('.main-chat__items').classList.toggle('main-chat__items-short');
+};
+
+//contacts list tabs
+doc.getElementById("contactsBtn").onclick = function () {
+    doc.querySelector('#contactsTab').classList.add('main-list__tab-active');
+    doc.querySelector('#invitationsTab').classList.remove('main-list__tab-active');
+
+    doc.querySelector('#contactsBtn').classList.add('main-list__button-active');
+    doc.querySelector('#invitationsBtn').classList.remove('main-list__button-active');
+};
+doc.getElementById("invitationsBtn").onclick = function () {
+    doc.querySelector('#invitationsTab').classList.add('main-list__tab-active');
+    doc.querySelector('#contactsTab').classList.remove('main-list__tab-active');
+
+    doc.querySelector('#invitationsBtn').classList.add('main-list__button-active');
+    doc.querySelector('#contactsBtn').classList.remove('main-list__button-active');
+};
